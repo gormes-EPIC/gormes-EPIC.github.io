322ea3e581f4e1db9b8dca769c6ac3ffd50c5c59
Gwyneth Ormes <134316348+gormes-EPIC@users.noreply.github.com>
2025-03-10 10:43:04 -0600

    split into AP and Pi's questions

--- piscript.js
+++ piscript.js
@@ -1,5 +1,5 @@
 async function loadQuestions() {
-    const response = await fetch('questions.csv');
+    const response = await fetch('pisquestions.csv');
     const data = await response.text();
     const rows = data.trim().split('\n'); 
 
