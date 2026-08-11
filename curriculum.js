fab3b5a247395a7a456030f533d16665a1c4b3b7
gormes-epic <gormes-epic@lps.k12.co.us>
2026-08-11 10:18:48 -0600

    fix indexing

--- curriculum.js
+++ curriculum.js
@@ -75,7 +75,7 @@ const curriculum = {
                 { file: "Blockbuster-Lab", type: "assignment" },
                 { file: "Style-Guides-and-Great-READMEs", type: "assignment"},
                 { file: "Advanced-Git-Lab", type: "assignment"},
-                { file: "Git Post-mortem-Discussion", type: "assignment"},
+                { file: "Git-Post-mortem-Discussion", type: "assignment"},
                 { file: "Currency-Translator-Lab", type: "assignment"},
                 { file: "Cowsay-ENV-Lab", type:"assignment"},
 
